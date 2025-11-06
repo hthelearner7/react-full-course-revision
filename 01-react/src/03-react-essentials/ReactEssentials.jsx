4b7fdf0cf7905593d0e34aeb17cfbf60ef9e9d34
Himanshu Juyal <himanshujuyal280404@gmail.com>
2025-11-07 00:42:34 +0530

    react-essentials

---
+++ 01-react/src/03-react-essentials/ReactEssentials.jsx
@@ -0,0 +1,28 @@
+import "./ReactEssentials.css";
+import reactCoreConceptsPng from "./assets/react-core-concepts.png";
+
+// making a separate component for header
+function Header() {
+    return (
+        <header>
+            <img src={reactCoreConceptsPng} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                Fundamental React concepts you will need for almost any app you
+                are going to build!
+            </p>
+        </header>
+    );
+}
+function ReactEssentials() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
+}
+
+export default ReactEssentials;
--- 01-react/src/App.jsx
+++ 01-react/src/App.jsx
@@ -1,10 +1,12 @@
-import "./App.css";
-import ReactCode from "./01-getting-started/01-react-vs-js-example/react/ReactCode";
+// import "./App.css";
+// import ReactCode from "./01-getting-started/01-react-vs-js-example/react/ReactCode";
+import ReactEssentials from "./03-react-essentials/ReactEssentials.jsx";
 
 export default function App() {
     return (
         <div>
-            <ReactCode />
+            {/* <ReactCode /> */}
+            <ReactEssentials />
         </div>
     );
 }
